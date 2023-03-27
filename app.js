4ca70042774f675c521a7e67d49e89a75e39c55a
Louis <cassany.louis@gmail.com>
2023-03-27 10:47:20 +0200

    Demo modes

--- app.js
+++ app.js
@@ -1,13 +1,79 @@
-let w = window.innerWidth * 0.95
-let h = window.innerHeight * 0.95
+window.addEventListener('load', (event) => {
+    const params = new Proxy(new URLSearchParams(window.location.search), {
+        get: (searchParams, prop) => searchParams.get(prop),
+    });
+    demo = params.demo; // "some_value"
+});
+
+let demo = null;
 let r = 300; // theter length
 let psi = 0;
 let phi = 0;
 let theta = 0;
 
-function mouseWheel(event) {
-    psi += event.delta / 1000;
+function setup() {
+    let cnv = createCanvas(windowWidth, windowHeight, WEBGL);
+    cnv.style('display', 'block');
+    rectMode(CENTER);
 }
+
+function mouseWheel(event) {
+    if (demo) psi += event.delta / 1000;
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+    background(0)
+    if (!demo) orbitControl(1, 1, 0)
+    translate(0, windowHeight / 5, 0)
+    rotateZ(PI / 2)
+    rotateY(PI / 2)
+    plotRepere(r)
+    if (demo == "manual") {
+        phi = map(mouseX, 0, windowWidth, PI / 2, -PI / 2)
+        theta = map(mouseY, windowHeight, 0, 0, PI / 2)
+    }
+    if (demo == "auto") {
+        theta = map(cos(frameCount / 100), -1, 1, radians(20), radians(50));
+        phi = map(sin(frameCount / 100), -1, 1, radians(-20), radians(70));
+    }
+    plotKite(phi, theta, psi, () => {
+        fill('magenta')
+        noStroke()
+        triangle(0, 50, 0, -50, -30, 0)
+    })
+    draw_arcs()
+}
+
+const plotRepere = (scale) => {
+    strokeWeight(2)
+    stroke("white")
+    line(0, 0, 0, scale, 0, 0)
+    stroke("white")
+    line(0, 0, 0, 0, -scale, 0)
+    line(0, 0, 0, 0, scale, 0)
+
+    // stroke("green")
+    // line(0, 0, 0, 0, 0, scale)
+}
+
+function plotKite(phi, theta, psi, plot) {
+    push()
+    translate(
+        r * cos(phi) * cos(theta),
+        r * sin(phi) * cos(theta),
+        r * -sin(theta)
+    )
+    rotateZ(phi);
+    rotateY(theta - PI / 2)
+    rotateZ(psi)
+    plot()
+    pop()
+}
+
 
 function draw_arcs() {
     noFill()
@@ -27,52 +93,4 @@ function draw_arcs() {
         arc(0, 0, 2 * r, 2 * r, 0, PI / 2)
         pop()
     }
-}
-
-function setup() {
-    createCanvas(w, h, WEBGL);
-    rectMode(CENTER);
-}
-
-function draw() {
-    background(0)
-    orbitControl(1, 1, 0)
-    translate(0, r / 3, 0)
-    rotateZ(PI / 2)
-    rotateY(PI / 2)
-    plotRepere(r)
-    phi = map(mouseX, 0, w, PI / 2, -PI / 2)
-    theta = map(mouseY, h, 0, 0, PI / 2)
-    // theta = map(cos(frameCount / 100), -1, 1, radians(20), radians(50));
-    // phi = map(sin(frameCount / 100), -1, 1, radians(-20), radians(70));
-    plotKite(phi, theta, psi, () => {
-        fill('magenta')
-        noStroke()
-        triangle(0, 50, 0, -50, -30, 0)
-    })
-    draw_arcs()
-}
-
-const plotRepere = (scale) => {
-    strokeWeight(2)
-    stroke("blue")
-    line(0, 0, 0, scale, 0, 0)
-    stroke("red")
-    line(0, 0, 0, 0, -scale, 0)
-    stroke("green")
-    line(0, 0, 0, 0, 0, scale)
-}
-
-function plotKite(phi, theta, psi, plot) {
-    push()
-    translate(
-        r * cos(phi) * cos(theta),
-        r * sin(phi) * cos(theta),
-        r * -sin(theta)
-    )
-    rotateZ(phi);
-    rotateY(theta - PI / 2)
-    rotateZ(psi)
-    plot()
-    pop()
 }
